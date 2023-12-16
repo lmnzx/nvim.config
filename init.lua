fe283ba910e36509e55b844458fd5a701b0b95fa
Sayan Mallick <lemon@Sayans-MacBook-Pro.local>
2023-12-16 20:02:23 +0530

    basic stuff

---
+++ init.lua
@@ -0,0 +1,33 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("core")
+require("lazy").setup({ import = "plugins" }, {
+    ui = {
+    icons = {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "🌎",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
+})
---
+++ lua/plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+    -- git related plugin
+    "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
+
+    -- detect tabstop and shistwidth automatically
+    "tpope/vim-sleuth",
+
+    -- copilot
+    "github/copilot.vim",
+}
