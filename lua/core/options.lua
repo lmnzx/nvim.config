606c33fd22cce9171901945f6c9d4d2080cce58f
Sayan Mallick <lemon@Sayans-MacBook-Pro.local>
2024-01-11 13:08:53 +0530

    befor lsp-zero

---
+++ lua/core/options.lua
@@ -0,0 +1,4 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.termguicolors = true
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,20 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-        signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-        },
-    },
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+				    add          = { text = '+' },
+				    change       = { text = '~' },
+				    delete       = { text = '_' },
+				    topdelete    = { text = '‾' },
+				    changedelete = { text = '~' },
+				    untracked    = { text = '|' },
+  				},
+				signcolumn = true, 
+				current_line_blame = true,
+			})
+		end,
+	},
 }
