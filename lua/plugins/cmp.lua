de2bfebbf725a042334e9dbebd1b17b20cd4cf6e
Sayan Mallick <snyxmk@gmail.com>
2024-05-10 11:36:02 +0530

    time to rewrite

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -30,7 +30,10 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
+
         mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
@@ -49,26 +52,6 @@ return {
               fallback()
             end
           end, { "i", "s" }),
-          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-          -- ["<Tab>"] = cmp.mapping(function(fallback)
-          --   if cmp.visible() then
-          --     cmp.select_next_item()
-          --   elseif luasnip.expand_or_jumpable() then
-          --     luasnip.expand_or_jump()
-          --   else
-          --     fallback()
-          --   end
-          -- end, { "i", "s" }),
-          -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-          --   if cmp.visible() then
-          --     cmp.select_prev_item()
-          --   elseif luasnip.jumpable(-1) then
-          --     luasnip.jump(-1)
-          --   else
-          --     fallback()
-          --   end
-          -- end, { "i", "s" }),
           ["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
           ["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
           ["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
