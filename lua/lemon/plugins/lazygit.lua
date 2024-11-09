f1d0f5fef00350d0fbfe57d92d31cd3944772bcd
lmnzx <snyxmk@gmail.com>
2024-11-09 18:53:43 +0530

    post-snacks

--- lua/lemon/plugins/lazygit.lua
+++ lua/lemon/plugins/lazygit.lua
@@ -1,16 +1,16 @@
 return {
-	"kdheepak/lazygit.nvim",
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	keys = {
-		{ "<leader>lg", "<cmd>LazyGit<cr>" },
-	},
+	-- "kdheepak/lazygit.nvim",
+	-- cmd = {
+	-- 	"LazyGit",
+	-- 	"LazyGitConfig",
+	-- 	"LazyGitCurrentFile",
+	-- 	"LazyGitFilter",
+	-- 	"LazyGitFilterCurrentFile",
+	-- },
+	-- dependencies = {
+	-- 	"nvim-lua/plenary.nvim",
+	-- },
+	-- keys = {
+	-- 	{ "<leader>lg", "<cmd>LazyGit<cr>" },
+	-- },
 }
