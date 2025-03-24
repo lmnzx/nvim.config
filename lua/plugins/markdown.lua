f13840947dd0b366efe9188f7760ded4847ce8df
Sayan Mallick <snyxmk@gmail.com>
2025-03-24 11:17:54 +0530

    done for a year

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,21 +2,21 @@ return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-		ft = "markdown",
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
 		opts = {
+			overrides = {
+				buftype = {
+					nofile = { enabled = false },
+				},
+			},
 			completions = { blink = { enabled = true } },
 			code = {
 				sign = false,
-				width = "block",
-				right_pad = 1,
-				style = "normal",
 			},
 			heading = {
 				sign = false,
 				position = "inline",
-				icons = {},
 			},
 		},
 	},
